beb66ebb452513776a0ff5692996bb6d71eaa2b0
Obi Chukwu <kennetho.onu@gmail.com>
2024-03-13 03:29:08 +0800

    console 0.1

--- console.py
+++ console.py
@@ -2,12 +2,14 @@
 ''' A Python Module Representing the Console '''
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     ''' Defines the command interpreter class '''
 
     prompt = "(hbnb) "
+    __classes = {"BaseModel"}
 
     def do_quit(self, arg):
         '''Quits command to exit the program
@@ -22,7 +24,20 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         ''' Allows an empty line to do nothing
         '''
-        return
+        pass
+
+    def do_create(self, arg):
+        '''Creates a new instance of BaseModel
+        '''
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            new_object = eval(f"{args[0]}")()
+            print(new_object.id)
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -3,13 +3,14 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
     ''' Defines the BaseModel Class '''
 
     def __init__(self, *args, **kwargs):
-        ''' Initializes the BaseModel class '''
+        ''' Initializes the BaseModel Class '''
 
         if len(kwargs) != 0:
             for key, value in kwargs.items():
@@ -24,21 +25,25 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         '''
-        Returns an informal string representation of the BaseModel Class
+        String representation of the BaseModel Class
         '''
+
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         ''' Updates the public instance attribute '''
         self.updated_at = datetime.now()
+        models.storage.save(self)
 
     def to_dict(self):
         '''
-        Returns a dictionary of all keys/values of __dict__ of the instance
+        Dictionary of all keys/values of __dict__ of the instance
         '''
+
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
